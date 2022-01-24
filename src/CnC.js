ddb6dbbb7e2d91aa4f5ebb072ada37f0e65d0150
Talkintomato <e0544532@u.nus.edu>
2022-01-24 18:43:23 +0800

    minor edits

--- src/CnC.js
+++ src/CnC.js
@@ -24,8 +24,9 @@ export default function Contact() {
 			subtitle1: {
 				fontFamily: "Adam",
 			},
-			h2: {
+			h3: {
 				fontFamily: "Adam",
+				fontWeight: "bold",
 			},
 			h6: {},
 		},
@@ -36,90 +37,104 @@ export default function Contact() {
 			{/* <Typography variant="h2" align="center" padding="30px" fontWeight="bold">
 				Cast & Creative
 			</Typography> */}
-			<Card
-				style={{
-					backgroundColor: "rgba(255,255,255,0.25)",
-					margin: "5%",
-					padding: "10px",
-				}}
-			>
-				<Grid container spacing={6}>
-					<Grid item xs={12} md={4}>
-						<img src={director} alt={"director"} loading="lazy" width="100%" />
-					</Grid>
+			<Grid container justifyContent="center">
+				<Grid Item md={10}>
+					<Card
+						style={{
+							backgroundColor: "rgba(255,255,255,0.25)",
+							margin: "5%",
+							padding: "10px",
+						}}
+					>
+						<Grid container spacing={6}>
+							<Grid item xs={12} md={4}>
+								<img
+									src={director}
+									alt={"director"}
+									loading="lazy"
+									width="100%"
+								/>
+							</Grid>
 
-					<Grid item xs={12} md={8}>
-						<Typography variant="h2" paddingTop="3%">
-							Director's Message
-						</Typography>
-						<Typography variant="h6">
-							Labyrinth Coldridge is a Temasek Hall musical, built completely
-							from scratch and only made possible through the tireless efforts
-							and collaboration with our talented cast members and production
-							crew. A product of endless nights of relentless meetings and
-							practices, Labyrinth Coldridge has braved through the COVID 19
-							pandemic to come to you this Feberuary.
-							<br />
-							<br /> Live from Gateway Theatre, Labyrinth Coldridge looks
-							forward to seeing you on 23, 24 February.
-							<br />
-							<br />- Noah Teo
-						</Typography>
-					</Grid>
-				</Grid>
-			</Card>
+							<Grid item xs={12} md={8}>
+								<Typography variant="h3" paddingTop="3%">
+									Director's Message
+								</Typography>
+								<Typography variant="h6">
+									Labyrinth Coldridge is a Temasek Hall musical, built
+									completely from scratch and only made possible through the
+									tireless efforts and collaboration with our talented cast
+									members and production crew. A product of endless nights of
+									relentless meetings and practices, Labyrinth Coldridge has
+									braved through the COVID 19 pandemic to come to you this
+									Feberuary.
+									<br />
+									<br /> Live from Gateway Theatre, Labyrinth Coldridge looks
+									forward to seeing you on 23, 24 February.
+									<br />
+									<br />- Noah Teo
+								</Typography>
+							</Grid>
+						</Grid>
+					</Card>
 
-			<Grid
-				container
-				justifyItems="center"
-				spacing={5}
-				style={{
-					padding: "5%",
-				}}
-			>
-				<Grid item xs={12} sm={12} textAlign="center">
-					<Typography variant="h3"> -CAST-</Typography>
-				</Grid>
-				{itemData.map((item) => (
-					<Grid item key={item.img} xs={6} sm={3}>
-						<img
-							src={`${item.img}`}
-							srcSet={`${item.img}`}
-							alt={item.title}
-							loading="lazy"
-							width="100%"
-						/>
-						<Typography variant="subtitle1" align="center" fontWeight="bold">
-							{item.title}
-						</Typography>
-						<Typography
-							variant="subtitle2"
-							align="center"
-							style={{ fontStyle: "oblique" }}
-						>
-							{item.role}
-						</Typography>
+					<Grid
+						container
+						justifyItems="center"
+						spacing={5}
+						style={{
+							padding: "5%",
+						}}
+					>
+						<Grid item xs={12} sm={12} textAlign="center">
+							<Typography variant="h3"> -CAST-</Typography>
+						</Grid>
+						{itemData.map((item) => (
+							<Grid item key={item.img} xs={6} sm={3}>
+								<img
+									src={`${item.img}`}
+									srcSet={`${item.img}`}
+									alt={item.title}
+									loading="lazy"
+									width="100%"
+								/>
+								<Typography
+									variant="subtitle1"
+									align="center"
+									fontWeight="bold"
+								>
+									{item.title}
+								</Typography>
+								<Typography
+									variant="subtitle2"
+									align="center"
+									style={{ fontStyle: "oblique" }}
+								>
+									{item.role}
+								</Typography>
+							</Grid>
+						))}
 					</Grid>
-				))}
-			</Grid>
-			<Grid
-				container
-				justifyItems="center"
-				justifyContent="center"
-				spacing={5}
-				style={{
-					padding: "5%",
-					marginBottom: "3vh",
-				}}
-			>
-				<Grid item xs={12} sm={12} textAlign="center">
-					<Typography variant="h3"> -Creative-</Typography>
-				</Grid>
-				<Grid xs={12} md={6}>
-					<CreativeTable rows={CreativeListOne}> </CreativeTable>
-				</Grid>
-				<Grid xs={12} md={6}>
-					<CreativeTable rows={CreativeListTwo}> </CreativeTable>
+					<Grid
+						container
+						justifyItems="center"
+						justifyContent="center"
+						spacing={5}
+						style={{
+							padding: "5%",
+							marginBottom: "3vh",
+						}}
+					>
+						<Grid item xs={12} sm={12} textAlign="center">
+							<Typography variant="h3"> -Creative-</Typography>
+						</Grid>
+						<Grid xs={12} md={6}>
+							<CreativeTable rows={CreativeListOne}> </CreativeTable>
+						</Grid>
+						<Grid xs={12} md={6}>
+							<CreativeTable rows={CreativeListTwo}> </CreativeTable>
+						</Grid>
+					</Grid>
 				</Grid>
 			</Grid>
 		</ThemeProvider>
--- src/Info.js
+++ src/Info.js
@@ -29,128 +29,133 @@ export default function Info() {
 
 	return (
 		<ThemeProvider theme={theme}>
-			<Grid
-				container
-				justifyContent="center"
-				paddingTop="5vh"
-				textAlign="center"
-			>
-				<Typography variant="h3" fontWeight="bold">
-					{" "}
-					SELECT SHOWDATE
-				</Typography>
-			</Grid>
 			<Grid container justifyContent="center">
-				<Button
-					variant="contained"
-					size="large"
-					style={{
-						margin: "3%",
-						backgroundColor: "#A9A9A9",
-						color: "rgb(0,0,0)",
-						fontFamily: "Adam",
-					}}
-				>
-					23 Feb 2022 TICKETS
-				</Button>
-				<Button
-					variant="contained"
-					size="large"
-					style={{
-						margin: "3%",
-						backgroundColor: "#A9A9A9",
-						color: "rgb(0,0,0)",
-						fontFamily: "Adam",
-					}}
-				>
-					24 Feb 2022 TICKETS
-				</Button>
-			</Grid>
-			<Paper
-				elevation={0}
-				style={{
-					padding: "1em ",
-					margin: "5%",
-					alignItems: "center",
-					backgroundColor: "transparent",
-					textAlign: "center",
-				}}
-			>
-				<Typography variant="h3" fontWeight="bold">
-					Sypnopsis
-				</Typography>
-				<Typography
-					variant="h6"
-					style={{
-						marginTop: "2em",
-						marginBottom: "10px",
-						fontFamily: "calibre",
-						fontWeight: "bold",
-					}}
-				>
-					Set in Coldridge, a female penitentiary, Labyrinth Coldridge tells the
-					tale of the life of Cassie, a girl who had been following the system
-					her entire life. As she slowly starts coming to terms with the
-					meaningless life that she has been sucked into, she finds herself in a
-					state of loss. Themed around introspection, friendship and finding
-					yourself, Labyrinth Coldridge hopes to keep you questioning if the
-					life you are currently living is the one you truly wish to pursue.
-				</Typography>
-			</Paper>
-			<Grid padding="1em" margin="5%" textAlign="center">
-				<Typography variant="h3" fontWeight="bold">
-					Tickets
-				</Typography>
-				<Prices></Prices>
-			</Grid>
-			<Card
-				style={{
-					// backgroundColor: "rgba(1,11,19,0.3)",
-					padding: "1em ",
-					margin: "5%",
-					marginBottom: "10vh",
-					alignItems: "center",
-					minHeight: "400px",
-					backgroundColor: "rgba(255,255,255,0.25)",
-				}}
-			>
-				<Grid container height="60vh">
-					<Grid item xs={12} sm={4}>
+				<Grid Item md={10} xs={13}>
+					<Grid
+						container
+						justifyContent="center"
+						paddingTop="5vh"
+						textAlign="center"
+					>
 						<Typography variant="h3" fontWeight="bold">
-							Location
+							{" "}
+							SELECT SHOWDATE
 						</Typography>
-						<Typography variant="h4" style={{ marginTop: "2%" }}>
-							Gateway Theatre
-						</Typography>
-						<Typography variant="h6">
-							3615 Jalan Bukit Merah, Singapore 159461
-						</Typography>
+					</Grid>
+					<Grid container justifyContent="center">
 						<Button
-							onClick={Directions}
 							variant="contained"
+							size="large"
 							style={{
-								marginTop: "10%",
-								marginBottom: "10%",
+								margin: "3%",
 								backgroundColor: "#A9A9A9",
 								color: "rgb(0,0,0)",
 								fontFamily: "Adam",
 							}}
-							xs={12}
 						>
-							Directions and Parking
+							23 Feb 2022 TICKETS
+						</Button>
+						<Button
+							variant="contained"
+							size="large"
+							style={{
+								margin: "3%",
+								backgroundColor: "#A9A9A9",
+								color: "rgb(0,0,0)",
+								fontFamily: "Adam",
+							}}
+						>
+							24 Feb 2022 TICKETS
 						</Button>
 					</Grid>
-					<Grid item xs={12} sm={8} minHeight="30vh">
-						<Iframe
-							src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15955.265653393968!2d103.8166283!3d1.2840557!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x995456882b346c19!2sGateway%20Theatre!5e0!3m2!1sen!2ssg!4v1640684123318!5m2!1sen!2ssg"
-							width="100%"
-							height="100%"
-							allowfullscreen=""
-							loading="lazy"
-						></Iframe>
+					<Paper
+						elevation={0}
+						style={{
+							padding: "1em ",
+							margin: "5%",
+							alignItems: "center",
+							backgroundColor: "transparent",
+							textAlign: "center",
+						}}
+					>
+						<Typography variant="h3" fontWeight="bold">
+							Sypnopsis
+						</Typography>
+						<Typography
+							variant="h6"
+							style={{
+								marginTop: "2em",
+								marginBottom: "10px",
+								fontFamily: "calibre",
+								fontWeight: "bold",
+							}}
+						>
+							Set in Coldridge, a female penitentiary, Labyrinth Coldridge tells
+							the tale of the life of Cassie, a girl who had been following the
+							system her entire life. As she slowly starts coming to terms with
+							the meaningless life that she has been sucked into, she finds
+							herself in a state of loss. Themed around introspection,
+							friendship and finding yourself, Labyrinth Coldridge hopes to keep
+							you questioning if the life you are currently living is the one
+							you truly wish to pursue.
+						</Typography>
+					</Paper>
+					<Grid padding="1em" margin="5%" textAlign="center">
+						<Typography variant="h3" fontWeight="bold">
+							Tickets
+						</Typography>
+						<Prices></Prices>
 					</Grid>
+					<Card
+						style={{
+							// backgroundColor: "rgba(1,11,19,0.3)",
+							padding: "1em ",
+							margin: "5%",
+							marginBottom: "10vh",
+							alignItems: "center",
+							minHeight: "400px",
+							backgroundColor: "rgba(255,255,255,0.25)",
+						}}
+					>
+						<Grid container height="60vh">
+							<Grid item xs={12} sm={4}>
+								<Typography variant="h3" fontWeight="bold">
+									Location
+								</Typography>
+								<Typography variant="h4" style={{ marginTop: "2%" }}>
+									Gateway Theatre
+								</Typography>
+								<Typography variant="h6">
+									3615 Jalan Bukit Merah, Singapore 159461
+								</Typography>
+								<Button
+									onClick={Directions}
+									variant="contained"
+									style={{
+										marginTop: "10%",
+										marginBottom: "10%",
+										backgroundColor: "#A9A9A9",
+										color: "rgb(0,0,0)",
+										fontFamily: "Adam",
+									}}
+									xs={12}
+								>
+									Directions and Parking
+								</Button>
+							</Grid>
+							<Grid item xs={12} sm={8} minHeight="30vh">
+								<Iframe
+									src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15955.265653393968!2d103.8166283!3d1.2840557!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x995456882b346c19!2sGateway%20Theatre!5e0!3m2!1sen!2ssg!4v1640684123318!5m2!1sen!2ssg"
+									width="100%"
+									height="100%"
+									allowfullscreen=""
+									loading="lazy"
+								></Iframe>
+							</Grid>
+						</Grid>
+					</Card>
 				</Grid>
-			</Card>
+			</Grid>
 		</ThemeProvider>
 	);
 }
